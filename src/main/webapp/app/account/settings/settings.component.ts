5890f7276d8bbd7da3010a5b50584c0d9566c561
Alexander Lvovskiy <alexander.lvovskiy@gmail.com>
2021-03-27 12:07:23 +0200

    Support of client tel change. In view and componment.ts.

--- src/main/webapp/app/account/settings/settings.component.ts
+++ src/main/webapp/app/account/settings/settings.component.ts
@@ -5,12 +5,15 @@ import { JhiLanguageService } from 'ng-jhipster';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
 import { LANGUAGES } from 'app/core/language/language.constants';
+import { Client, IClient } from 'app/shared/model/client.model';
+import { ClientService } from 'app/entities/client/client.service';
 
 @Component({
   selector: 'jhi-settings',
   templateUrl: './settings.component.html',
 })
 export class SettingsComponent implements OnInit {
+  client: IClient = new Client();
   account!: Account;
   success = false;
   languages = LANGUAGES;
@@ -18,10 +21,16 @@ export class SettingsComponent implements OnInit {
     firstName: [undefined, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
     lastName: [undefined, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
     email: [undefined, [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
+    tel: [null, [Validators.required, Validators.minLength(0), Validators.maxLength(15)]],
     langKey: [undefined],
   });
 
-  constructor(private accountService: AccountService, private fb: FormBuilder, private languageService: JhiLanguageService) {}
+  constructor(
+    private accountService: AccountService,
+    private clientService: ClientService,
+    private fb: FormBuilder,
+    private languageService: JhiLanguageService
+  ) {}
 
   ngOnInit(): void {
     this.accountService.identity().subscribe(account => {
@@ -36,6 +45,19 @@ export class SettingsComponent implements OnInit {
         this.account = account;
       }
     });
+    this.clientService.findByUserId(this.account.id).subscribe(client => {
+      if (client) {
+        this.settingsForm.patchValue({
+          tel: client.body?.tel,
+        });
+
+        (this.client.id = client.body?.id),
+          (this.client.isShopOwner = client.body?.isShopOwner),
+          (this.client.shops = client.body?.shops),
+          (this.client.tel = client.body?.tel),
+          (this.client.user = client.body?.user);
+      }
+    });
   }
 
   save(): void {
@@ -46,14 +68,18 @@ export class SettingsComponent implements OnInit {
     this.account.email = this.settingsForm.get('email')!.value;
     this.account.langKey = this.settingsForm.get('langKey')!.value;
 
-    this.accountService.save(this.account).subscribe(() => {
-      this.success = true;
+    this.client.tel = this.settingsForm.get('tel')!.value;
 
+    this.accountService.save(this.account).subscribe(() => {
       this.accountService.authenticate(this.account);
 
       if (this.account.langKey !== this.languageService.getCurrentLanguage()) {
         this.languageService.changeLanguage(this.account.langKey);
       }
+    });
+
+    this.clientService.update(this.client).subscribe(() => {
+      this.success = true;
     });
   }
 }
